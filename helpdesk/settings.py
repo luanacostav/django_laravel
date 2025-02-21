dcb8f746557144afbe6b07aedfe574425cb2330c
luanacostav <costavasconcelosluana@gmail.com>
2025-02-21 18:47:42 -0300

    mudança no settings

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2dlz*g0grdhbn)i+eb#w@4n0x_w%)m))w@w*jo-b6g(22gf*2)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
